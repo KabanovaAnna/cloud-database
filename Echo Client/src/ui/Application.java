c97cd2b345d425d5414ea4e91673c8631973ceee
Dmytro Rybalko <ga24yow@tum.de>
2016-10-22 15:01:22 +0200

    add logerInitializer method

--- Echo Client/src/ui/Application.java
+++ Echo Client/src/ui/Application.java
@@ -1,9 +1,14 @@
 package ui;
 
+import org.apache.log4j.Logger;
+
 public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		LoggerInitializer li = new LoggerInitializer();
+		Logger mylogger = li.initializeLogger();
+		mylogger.info("my first log");
 	}
 	
 }
--- Echo Client/src/ui/LoggerInitializer.java
+++ Echo Client/src/ui/LoggerInitializer.java
@@ -1,11 +1,30 @@
 package ui;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
+
+import org.apache.log4j.*;
 
 public class LoggerInitializer {
 
-	public void initializeLogger() {
-		Logger mylogger = Logger.getLogger(Application.class); 
+	private Logger logger;
+
+	public Logger initializeLogger() {
+		logger = Logger.getLogger(Application.class); 
+		logger.setLevel(Level.ALL);
+		SimpleLayout sl = new SimpleLayout();
+		ConsoleAppender consoleAppender = new ConsoleAppender(sl);
+		logger.addAppender(consoleAppender);
+		String logDir = "logs/client.log";
+		String pattern = "%d{ISO8601} %-5p [%t] %c: %m%n";
+		PatternLayout pLayout = new PatternLayout(pattern);
+		FileAppender fa;
+		try {
+			fa = new FileAppender(pLayout, logDir, true);
+			logger.addAppender(fa);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return logger;
 	}
 
 }
