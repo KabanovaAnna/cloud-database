bcc155b4d603f3b9b2dbc699378cec01239351a4
Dmytro Rybalko <ga24yow@tum.de>
2016-10-26 20:07:02 +0200

    merge master

--- Echo Client/src/ui/Application.java
+++ Echo Client/src/ui/Application.java
@@ -1,16 +1,8 @@
 package ui;
 
-import org.apache.log4j.*;
-
 public class Application {
 
 	public static void main(String[] args) throws Exception {
-		initializeLogger();
 		CommandLineProcessor.readInput();
-	}
-	
-	private static void initializeLogger() {
-		Logger logger = LoggerInitializer.initialize(Application.class);
-		logger.info("Application started");
 	}
 }
