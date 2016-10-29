4bf63d1dec5da1ec9a834fda11723693b21f9016
Dmytro Rybalko <ga24yow@tum.de>
2016-10-29 23:15:41 +0200

    Node and Comparator for lfu cache

--- BasicStorageServer-stub/src/app_kvServer/cache/LfuCacheStrategy.java
+++ BasicStorageServer-stub/src/app_kvServer/cache/LfuCacheStrategy.java
@@ -1,5 +1,34 @@
 package app_kvServer.cache;
 
-public class LfuCacheStrategy {
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class LfuCacheStrategy implements CacheStrategy {
+
+	private PriorityQueue<LfuQueueNode> queue;
+	private HashMap<String, String> kvPairs;
+	
+	public LfuCacheStrategy() {
+		queue = new PriorityQueue<LfuQueueNode>(new LfuQueueNodeComparator());
+	}
+
+	@Override
+	public boolean addElement(String key, String value) {
+		
+	}
+	
+	public void updateElement() {
+		
+	}
+
+	@Override
+	public String getValueFor(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void deleteElement() {
+
+	}
 
 }
--- BasicStorageServer-stub/src/app_kvServer/cache/LruCacheStrategy.java
+++ BasicStorageServer-stub/src/app_kvServer/cache/LruCacheStrategy.java
@@ -1,5 +1,17 @@
 package app_kvServer.cache;
 
-public class LruCacheStrategy {
+public class LruCacheStrategy implements CacheStrategy {
+
+	@Override
+	public void addElement(String key, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getValueFor(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
